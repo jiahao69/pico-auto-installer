9b4feaa1855849b8cf1db630e581f61f982d1b54
jinjiahao <2631714340@qq.com>
2025-05-10 15:29:12 +0800

    chore:移除获取adb版本函数

--- src/main/utils/adb-helper.ts
+++ src/main/utils/adb-helper.ts
@@ -2,7 +2,6 @@ import path from 'path'
 import process from 'process'
 
 import adbBin from '../../../resources/adb/adb.exe?asset&asarUnpack'
-import { executeCommand } from './execute-command'
 
 // 设置adb环境变量
 export function setupAdbInPath() {
@@ -14,15 +13,4 @@ export function setupAdbInPath() {
   if (platform === 'win32') {
     process.env.PATH = `${adbDir}${path.delimiter}${process.env.PATH}`
   }
-}
-
-// 获取adb版本号
-export async function getAdbVersion() {
-  const versionInfo = await executeCommand('adb version')
-
-  // 在版本信息中截取版本号
-  const pattern = /Android Debug Bridge version (\d+\.\d+\.\d+)/
-  const version = versionInfo.match(pattern)?.[1] || ''
-
-  return version
 }
